a0cc617f9030c90961c9d55b6561a00b1d466195
feittt <nwgale@gmail.com>
2025-08-24 13:24:45 +0800

    左对刘+使用title值

--- themes/simpleFeittt/components/Header.js
+++ themes/simpleFeittt/components/Header.js
@@ -12,7 +12,7 @@ export default function Header(props) {
   const { siteInfo } = props
   
   // 获取网站标题和描述
-  const siteTitle = siteConfig('AUTHOR') // 使用AUTHOR作为网站标题
+  const siteTitle = siteConfig('TITLE') // 使用TITLE作为网站标题
   const siteDescription = siteConfig('DESCRIPTION')
   
   // 背景图片URL，如果没有设置则使用默认颜色
@@ -22,16 +22,18 @@ export default function Header(props) {
     <header className='relative z-10'>
       {/* 顶部背景区域 */}
       <div 
-        className='w-full py-16 text-center bg-pink-200 dark:bg-pink-900 relative'
+        className='w-full py-16 bg-pink-200 dark:bg-pink-900 relative'
         style={bgImageUrl ? { 
           backgroundImage: `url(${bgImageUrl})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center'
         } : {}}
       >
-        <div className='container mx-auto px-6'>
-          <h1 className='text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4'>{siteTitle || 'My Website'}</h1>
-          <p className='text-xl text-gray-600 dark:text-gray-300'>{siteDescription}</p>
+        <div className='container mx-auto'>
+          <div className='px-6 md:px-10 lg:px-14 xl:px-16'>
+            <h1 className='text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4 text-left'>{siteTitle || 'My Website'}</h1>
+            <p className='text-xl text-gray-600 dark:text-gray-300 text-left'>{siteDescription}</p>
+          </div>
         </div>
       </div>
     </header>
