2c612092c61632912d0bf4693598c7e7e0f0d7f6
feittt <nwgale@gmail.com>
2025-08-24 12:56:41 +0800

    改变了主题

--- themes/simpleFeittt/components/Header.js
+++ themes/simpleFeittt/components/Header.js
@@ -10,9 +10,33 @@ import SocialButton from './SocialButton'
  */
 export default function Header(props) {
   const { siteInfo } = props
+  
+  // 获取网站标题和描述
+  const siteTitle = siteConfig('TITLE')
+  const siteDescription = siteConfig('DESCRIPTION')
+  
+  // 背景图片URL，如果没有设置则使用默认颜色
+  const bgImageUrl = siteConfig('HEADER_BACKGROUND_IMG') || null
 
   return (
-    <header className='text-center justify-between items-center px-6 bg-white h-80 dark:bg-black relative z-10'>
+    <header className='relative z-10'>
+      {/* 顶部背景区域 */}
+      <div 
+        className='w-full py-16 text-center bg-pink-200 dark:bg-pink-900 relative'
+        style={bgImageUrl ? { 
+          backgroundImage: `url(${bgImageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        } : {}}
+      >
+        <div className='container mx-auto px-6'>
+          <h1 className='text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4'>{siteTitle || 'My Website'}</h1>
+          <p className='text-xl text-gray-600 dark:text-gray-300'>{siteDescription}</p>
+        </div>
+      </div>
+      
+      {/* 原有的头部内容 */}
+      <div className='text-center justify-between items-center px-6 bg-white dark:bg-black'>
       <div className='float-none inline-block py-12'>
         <SmartLink href='/'>
           {/* 可使用一张单图作为logo */}
