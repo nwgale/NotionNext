015ba4739d7cd2163c6177c351c22be1e9e75358
feittt <nwgale@gmail.com>
2025-08-24 13:16:21 +0800

    Update Header.js

--- themes/simpleFeittt/components/Header.js
+++ themes/simpleFeittt/components/Header.js
@@ -34,46 +34,6 @@ export default function Header(props) {
           <p className='text-xl text-gray-600 dark:text-gray-300'>{siteDescription}</p>
         </div>
       </div>
-      
-      {/* 原有的头部内容 */}
-      <div className='text-center justify-between items-center px-6 bg-white dark:bg-black'>
-        <div className='float-none inline-block py-12'>
-          <SmartLink href='/'>
-          {/* 可使用一张单图作为logo */}
-          <div className='flex space-x-6 justify-center'>
-            <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer justify-center items-center flex'>
-              <LazyImage
-                priority={true}
-                src={siteInfo?.icon}
-                className='rounded-full'
-                width={100}
-                height={100}
-                alt={siteConfig('AUTHOR')}
-              />
-            </div>
-
-            <div className='flex-col flex justify-center'>
-              <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>
-                {siteConfig('AUTHOR')}
-              </div>
-              <div
-                className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'
-                dangerouslySetInnerHTML={{
-                  __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG)
-                }}
-              />
-            </div>
-          </div>
-        </SmartLink>
-
-          <div className='flex justify-center'>
-            <SocialButton />
-          </div>
-          <div className='text-xs mt-4 text-gray-500 dark:text-gray-300'>
-            {siteConfig('DESCRIPTION')}
-          </div>
-        </div>
-      </div>
     </header>
   )
 }
